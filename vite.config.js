4d809178beba37261c339d5d75aeb842dc68d2dc
Sabiq <sabiqhabiburrahman@gmail.com>
2025-11-15 13:47:31 +0700

    y

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,54 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // App files (auth, profile, dll)
                 "resources/css/app.css",
                 "resources/js/app.js",
+
+                // Welcome page
                 "resources/css/welcome.css",
                 "resources/js/welcome.js",
+
+                // Dashboard Admin files
+                "resources/js/dashboard-admin.js",
+                "resources/css/dashboard-admin-layout.css",
+                "resources/css/dashboard-admin-components.css",
+                "resources/css/dashboard-admin-responsive.css",
             ],
             refresh: true,
         }),
     ],
     build: {
         manifest: true,
-        outDir: "public/build", // Pastikan ini sesuai
+        outDir: "public/build",
         emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+                entryFileNames: "assets/[name]-[hash].js",
+                chunkFileNames: "assets/[name]-[hash].js",
+                assetFileNames: "assets/[name]-[hash].[ext]",
+            },
+        },
+        chunkSizeWarningLimit: 1000,
+        sourcemap: false, // Disable sourcemap untuk production
+        minify: "esbuild",
+    },
+    optimizeDeps: {
+        include: ["axios", "alpinejs"],
+    },
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+            "~": "/resources/css",
+        },
+    },
+    server: {
+        host: "localhost",
+        port: 5173,
+        strictPort: false,
+        hmr: {
+            host: "localhost",
+        },
     },
 });
